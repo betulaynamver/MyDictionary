fcc00e3f5dc45cdeffcdf380cd03601d01d65fc2
Betülay Namver <80717672+betulaynamver@users.noreply.github.com>
2021-03-21 21:25:46 +0300

    Add project files.

---
+++ MyDictionary/Dictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class Dictionary<TKey, TValue>
+    {
+        TKey[] Numara;
+        TValue[] Cicek;
+        public Dictionary()
+        {
+            Numara = new TKey[0];
+            Cicek = new TValue[0];
+        }
+        public int Length { get; internal set; }
+
+        public void Add(TKey numara, TValue cicek)
+        {
+            TKey[] tempKey = Numara;
+            TValue[] tempValue = Cicek;
+
+            Numara = new TKey[Numara.Length + 1];
+            Cicek = new TValue[Cicek.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                Numara[i] = tempKey[i];
+            }
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                Cicek[i] = tempValue[i];
+            }
+            Numara[Numara.Length - 1] = numara;
+            Cicek[Cicek.Length - 1] = cicek;
+            //Console.WriteLine(numara);
+            //Console.WriteLine(cicek);
+            Console.WriteLine("Çiçek kodu, İsmi: " + numara + " , "+ cicek);
+        }
+        public int Count
+        {
+            get { return Numara.Length; }
+        }
+       
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Dictionary sınıfı, içinde benim belirlediğim bir key'e uygun olarak değer saklar.
+             Zorunluluğu vardır:
+            -Key türünü belirlememiz şarttır. (int, string, float...) */
+
+            Dictionary<int, string> Cicek = new Dictionary<int, string>();
+            //Veri ekliyoruz.
+            Cicek.Add(101, "Gül");
+            Cicek.Add(103, "Papatya");
+            Cicek.Add(104, "Lale");
+
+            var eleman = Cicek.Count; //Değer sayma özelliği vardır.
+
+            Console.WriteLine("Çiçek çeşidi: " + Cicek.Count);
+
+            for (int i = 0; i < Cicek.Length; i++)
+            {
+                Console.WriteLine(Cicek);
+            }
+        }
+        
+    }
+}
